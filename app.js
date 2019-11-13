7d78bef400df99e97e4b278bfe65103205ce9ab8
Sachin Duhan <duhan.sachin2610@gmail.com>
2019-11-14 01:08:24 +0530

    initial setup of the application

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const bodyParser = require('body-parser');
+
+// CORS error : 
+app.use(cors());
+app.options('*', cors());
+
+// setting up the middle ware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+const email = require('./routes/email');
+app.use('email', email);
+
+// error handling
+app.use((req, res, next) => {
+    const error = new Error('Invalid Request');
+    res.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status = error.status || 500;
+    return res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
+module.exports = app;
---
+++ routes/email.js
@@ -0,0 +1,8 @@
+const express = require('express'),
+    Router = express.Router();
+
+Router.post('/', (req, res) => {
+
+})
+
+module.exports = Router;
